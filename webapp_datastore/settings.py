608157178c95ef8b5b844194070cc71af1dc864d
Mees, T.D <t.d.mees@uu.nl>
2020-09-24 16:48:23 +0200

    Fixed switching languages not working

--- webapp_datastore/settings.py
+++ webapp_datastore/settings.py
@@ -80,6 +80,7 @@ LOGIN_URL = reverse_lazy('main:login')
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
