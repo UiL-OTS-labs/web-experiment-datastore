f14fc13381e275c94ee795fcb39e0f932f145394
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2020-10-07 14:03:40 +0200

    Added a confirmation mail for users

--- experiments/utils/mails.py
+++ experiments/utils/mails.py
@@ -9,10 +9,23 @@ def send_new_experiment_mail(experiment: Experiment, user: User) -> None:
     send_template_email(
         [settings.LABSTAFF_EMAIL],
         "New experiment",
-        "experiments/mail/new_experiment",
+        "experiments/mail/new_experiment_staff",
         {
             "experiment": experiment,
             "user": user
         },
         language='en',
+    )
+
+    recipient_list = [user.email for user in experiment.users.all()]
+    send_template_email(
+        recipient_list,
+        "Confirmation new experiment",
+        "experiments/mail/new_experiment_user",
+        {
+            "experiment": experiment,
+            "user": user
+        },
+        language='en',
+        from_email=settings.LABSTAFF_EMAIL,
     )
