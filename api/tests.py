b7bf9f854b13ec44c1878e0a6070f6e87ee543b8
bbonf <bonfil@gmail.com>
2022-01-12 13:20:36 +0100

    fix for i18n in api test

--- api/tests.py
+++ api/tests.py
@@ -2,6 +2,7 @@ import json
 import uuid
 
 from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 from rest_framework.test import APITestCase
 
 from experiments.models import Experiment
@@ -58,7 +59,7 @@ class TestExperimentApi(APITestCase):
 
         response = self.client.get(reverse('api:metadata', args=[self.exp.access_id]))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()['state'], 'Open')
+        self.assertEqual(response.json()['state'], _("experiments:models:experiment:state:open"))
 
     def test_get_metadata_not_found(self):
         response = self.client.get(reverse('api:metadata', args=[uuid.uuid4()]))
