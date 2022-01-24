3cdae892c98378472c57814dffc40a2d12589cbd
bbonf <b.bonfil@uu.nl>
2022-01-24 14:10:23 +0100

    moved embedded script to js file

---
+++ experiments/static/experiments/js/edit.js
@@ -0,0 +1,19 @@
+function hideExtraForms(existingGroups) {
+    $('.target-group-form').each((idx, el) => {
+        if (idx >= existingGroups) {
+            $(el).hide();
+        }
+    });
+}
+
+$(function() {
+    $('#add_group').click((event) => {
+        event.preventDefault();
+        let hidden = $('.target-group-form:hidden');
+        hidden.last().show();
+
+        if (hidden.length - 1 <= 0) {
+            $('#add_group').hide();
+        }
+    });
+});
