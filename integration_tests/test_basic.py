d5d9c24dee0168f756cf39f491cfbaf8cfd320d0
Ashly <a.n.pieterman@students.uu.nl>
2025-04-24 12:42:27 +0200

    fixed test and added extra datapoints

--- integration_tests/test_basic.py
+++ integration_tests/test_basic.py
@@ -22,32 +22,48 @@ def test_admin_menu(as_admin):
 
 
 def test_data_points_added_chronologically(as_admin, admin_user, db, live_server):
-   
+    """
+    Test that DataPoints are stored in chronological order by 'date_added' timestamp.
+    """
     exp = Experiment.objects.create(access_id=uuid.uuid4()) 
     page = as_admin
     
 
     exp.users.add(admin_user)
    
-
+  
 
     # Create DataPoints
     dp1 = DataPoint.objects.create(experiment=exp)
+    dp1.date_added = datetime(2023, 10, 3, 11, 35)
+    dp1.save()
     dp2 = DataPoint.objects.create(experiment=exp)
+    dp2.date_added = datetime(2024, 10, 3, 23, 23)
+    dp2.save()
     dp3 = DataPoint.objects.create(experiment=exp)
-    """
-    Test that DataPoints are stored in chronological order by 'date_added' timestamp.
-    """
-    page.goto(live_server.url+ '/experiments/1/')
+    dp3.date_added = datetime(2024, 10, 11, 17, 21)
+    dp3.save()
+    dp4 = DataPoint.objects.create(experiment=exp) 
+    dp4.date_added = datetime(2024, 10, 23, 1, 35)
+    dp4.save()
+    dp5 = DataPoint.objects.create(experiment=exp) 
+    dp5.date_added = datetime(2024, 10, 23, 2, 35)
+    dp5.save()
+    dp6 = DataPoint.objects.create(experiment=exp) 
+    dp6.date_added = datetime(2024, 10, 23, 11, 35)
+    dp6.save()
+    #Bug 85 replicate
+
+    page.goto(f'{live_server.url}/experiments/{exp.id}/')
     page.locator("th").nth(3).click()
-    
+
 
     rows = page.locator("#DataTables_Table_0 tbody tr")
     rendered_dates = [
         rows.nth(i).locator("td").nth(3).inner_text() for i in range(rows.count())
     ]
 
-    expected = sorted([dp1, dp2, dp3], key=lambda dp: dp.date_added)
+    expected = sorted([dp1, dp2, dp3, dp4, dp5, dp6], key=lambda dp: dp.date_added, reverse=True) #Reverse because it started comparing ascending to the descending list
     expected_dates = [defaultfilters.date(dp.date_added)+ ", " + defaultfilters.time(dp.date_added) for dp in expected]
 
     assert rendered_dates == expected_dates
