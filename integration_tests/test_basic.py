71cab40bd984bdb8d25050e780c9783cf38a38eb
Ben <b.bonfil@uu.nl>
2025-05-26 14:39:01 +0200

    updated integration test

--- integration_tests/test_basic.py
+++ integration_tests/test_basic.py
@@ -26,31 +26,29 @@ def test_data_points_added_chronologically(as_admin, admin_user, db, live_server
     """
     Test that DataPoints are stored in chronological order by 'date_added' timestamp.
     """
-    exp = Experiment.objects.create(access_id=uuid.uuid4()) 
+    exp = Experiment.objects.create(access_id=uuid.uuid4())
     page = as_admin
-    
+
 
     exp.users.add(admin_user)
-   
-  
 
     # Create DataPoints
-    dp1 = DataPoint.objects.create(experiment=exp)
+    dp1 = DataPoint.objects.create(experiment=exp, data='')
     dp1.date_added = datetime(2023, 10, 3, 11, 35)
     dp1.save()
-    dp2 = DataPoint.objects.create(experiment=exp)
+    dp2 = DataPoint.objects.create(experiment=exp, data='')
     dp2.date_added = datetime(2024, 10, 3, 23, 23)
     dp2.save()
-    dp3 = DataPoint.objects.create(experiment=exp)
+    dp3 = DataPoint.objects.create(experiment=exp, data='')
     dp3.date_added = datetime(2024, 10, 11, 17, 21)
     dp3.save()
-    dp4 = DataPoint.objects.create(experiment=exp) 
+    dp4 = DataPoint.objects.create(experiment=exp, data='')
     dp4.date_added = datetime(2024, 10, 23, 1, 35)
     dp4.save()
-    dp5 = DataPoint.objects.create(experiment=exp) 
+    dp5 = DataPoint.objects.create(experiment=exp, data='')
     dp5.date_added = datetime(2024, 10, 23, 2, 35)
     dp5.save()
-    dp6 = DataPoint.objects.create(experiment=exp) 
+    dp6 = DataPoint.objects.create(experiment=exp, data='')
     dp6.date_added = datetime(2024, 10, 23, 11, 35)
     dp6.save()
     #Bug 85 replicate
@@ -58,12 +56,12 @@ def test_data_points_added_chronologically(as_admin, admin_user, db, live_server
 
     page.goto(f'{live_server.url}/experiments/{exp.id}/')
     time.sleep(1)
-    page.locator("#DataTables_Table_0 th").nth(3).click()
+    page.locator("#DataTables_Table_0 th").nth(4).click()
 
 
     rows = page.locator("#DataTables_Table_0 tbody tr")
     rendered_dates = [
-        rows.nth(i).locator("td").nth(3).inner_text() for i in range(rows.count())
+        rows.nth(i).locator("td").nth(4).inner_text() for i in range(rows.count())
     ]
 
     expected = sorted([dp1, dp2, dp3, dp4, dp5, dp6], key=lambda dp: dp.date_added) #Reverse because it started comparing ascending to the descending list
