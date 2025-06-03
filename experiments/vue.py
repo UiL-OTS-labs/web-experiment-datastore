845adcc45ab2295578d70885e39f2262d58df5fa
Ben <b.bonfil@uu.nl>
2025-06-03 09:57:55 +0200

    removed vue experiment list

--- experiments/vue.py
+++
@@ -1,8 +0,0 @@
-from cdh.vue.components import VueComponent, Vue
-
-Vue.add_component(VueComponent(
-    'ExperimentList',
-    location='experiments/vue/ExperimentList.vue',
-    subcomponents=[],
-    depends=['FancyList']
-))
