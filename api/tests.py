02e91ceb1e0b967897fa8637348499712941b953
ben <b.bonfil@uu.nl>
2026-04-23 16:37:30 +0200

    changed files-related test to use TransactionTestCase so that on_commit hook is called

--- api/tests.py
+++ api/tests.py
@@ -5,7 +5,7 @@ import random
 import uuid
 
 from django.urls import reverse
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APITransactionTestCase
 
 from experiments.models import Experiment, ParticipantSession
 from .views import ResultCodes
@@ -70,20 +70,19 @@ class TestExperimentApi(APITestCase):
         self.assertEqual(response.json()['result'], ResultCodes.ERR_UNKNOWN_ID)
 
 
-class TestTargetGroupAllocation(APITestCase):
-    @classmethod
-    def setUpTestData(cls):
+class TestTargetGroupAllocation(APITransactionTestCase):
+    def setUp(self):
         access_id = uuid.uuid4()
-        cls.exp = Experiment.objects.create(
+        self.exp = Experiment.objects.create(
             access_id=access_id,
             state=Experiment.OPEN,
             approved=True
         )
         # Remove the "default" group as that feature is newer that this tests"
-        cls.exp.targetgroup_set.all().delete()
+        self.exp.targetgroup_set.all().delete()
         # And Run the test as The tests were designed
-        cls.group_a = cls.exp.targetgroup_set.create(name='A', completion_target=2)
-        cls.group_b = cls.exp.targetgroup_set.create(name='B', completion_target=2)
+        self.group_a = self.exp.targetgroup_set.create(name='A', completion_target=2)
+        self.group_b = self.exp.targetgroup_set.create(name='B', completion_target=2)
 
     def test_create_participant(self):
         response = self.client.post(reverse('api:participant', args=[self.exp.access_id]))
